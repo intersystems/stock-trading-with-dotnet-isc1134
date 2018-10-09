df57dcc3a47390c058aba99869ec31ada95a38f8
duongdo27 <dohoangduong95@gmail.com>
2018-10-09 15:02:58 -0400

    Update to combine multiple tasks into single file

---
+++ Solutions/XEP/Trade.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Numerics;
+
+namespace myApp{
+    class Trade{
+        public String stockName;
+        public DateTime purchaseDate;
+        public double purchasePrice;
+        int shares;
+        String traderName;
+        public Trade(){
+
+        }
+
+        public Trade(String stockName, DateTime purchaseDate, double purchasePrice, int shares, String traderName) { 
+            //super();
+            this.stockName = stockName;
+            this.purchaseDate = purchaseDate;
+            this.purchasePrice = purchasePrice;
+            this.shares = shares;
+            this.traderName = traderName;
+        }
+
+        public static Trade[] generateSampleData(int objectCount) {
+            Trade[] data = new Trade[objectCount];
+            try{
+            
+            for (int i=0;i<objectCount;i++) {
+                DateTime tempDate = Convert.ToDateTime("2018/01/01");
+                double tempPrice = 25;
+                
+                data[i] = new Trade("XXX", tempDate, tempPrice, 5, "TestTrader"); 
+            }
+            
+        }
+        catch (Exception e) {
+            Console.WriteLine("Interactive prompt failed:\n" + e);
+        }
+            return data;
+        }
+    }
+}
--- Solutions/XEP/xepplaystocks.cs
+++ Solutions/XEP/xepplaystocks.cs
@@ -1,20 +1,20 @@
-﻿using System;
+
+using System;
 using InterSystems.Data.IRISClient;
 using InterSystems.XEP;
 using System.Data.SqlClient;
 using System.Data;
 
-
 namespace myApp
 {
-    class xepplaystocksTask6
+    class xepplaystocks
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            String host = "104.197.75.13";
-            int port = 21652;
+            String host = "localhost";
+            int port = 182;
             String username = "SuperUser";
             String password = "SYS";
             String Namespace = "USER";
@@ -40,91 +40,41 @@ namespace myApp
 				Console.WriteLine("2. Confirm all trades");
 				Console.WriteLine("3. Generate and save multiple trades");
 				Console.WriteLine("4. Retrieve all trades; show execution statistics");
-				Console.WriteLine("5. ADO.NET Comparison - Create and save multiple trades");
+				Console.WriteLine("5. JDBC Comparison - Create and save multiple trades");
 				Console.WriteLine("6. Quit");
 				Console.WriteLine("What would you like to do? ");
 				
 				String option = Console.ReadLine();
 				switch (option) {
-				case "1":
-					//Create trade object
-					Console.WriteLine("Stock name: ");
-					String name = Console.ReadLine();
-					
-					Console.WriteLine("Date (YYYY/MM/DD): ");
-					DateTime date;
-					if (DateTime.TryParse(Console.ReadLine(), out date)){
-						
-					}
-					else{
-						Console.WriteLine("Invalid date format!");
-						break;
-					}  
-										
-					Console.WriteLine("Price: ");
-					String inputPrice = Console.ReadLine();
-                    double price;
-                    double.TryParse(inputPrice, out price);
-					if (price <= 0){
-						Console.WriteLine("Price has to bigger than 0");
-						break;
-					}
-
-					Console.WriteLine("Number of Shares: ");
-					String inputShare = Console.ReadLine();
-                    int shares;
-                    Int32.TryParse(inputShare, out shares);
-					if (shares <= 0){
-						Console.WriteLine("Number of Shares has to bigger than 0");
-						break;
-					}
-					Console.WriteLine("Trader name: ");
-					String traderName = Console.ReadLine();
-					
-					sampleArray = CreateTrade(name, date, price, shares, traderName, sampleArray);
+				
+                // Task 2
+                case "1":
+                    // uncomment below line to run Task 2 - Create Trade 
+                    // Task2CreateTrade(sampleArray);
 					break;
 				case "2":
-					//Save trades
-					Console.WriteLine("Saving trades.");
-					XEPSaveTrades(sampleArray, xepEvent);
-					sampleArray = null;
+                    // uncomment below line to run Task 2 - Save Trade 
+                    // Task2SaveTrade(sampleArray);
 					break;
+                
+                // Task 3
 				case "3":
-					Console.WriteLine("How many items do you want to generate? ");	
-					String inputNumber = Console.ReadLine();
-                    int number;
-                    Int32.TryParse(inputNumber, out number);
-					if (number <= 0){
-						Console.WriteLine("Number of items has to bigger than 0");
-						break;
-					}
-					//Get sample generated array to store
-					sampleArray = Trade.generateSampleData(number);
-					
-					//Save generated trades
-					long totalStore = XEPSaveTrades(sampleArray,xepEvent);
-					Console.WriteLine("Execution time: " + totalStore + "ms");
+                    // uncomment below line to run Task 3
+                    //Task3(sampleArray, xepEvent);
 					break;
+                
+                // Task 5 + Task 6  
 				case "4":
-					Console.WriteLine("Fetching all. Please wait...");
-					long totalFetch = ViewAll(xepEvent);
-					Console.WriteLine("Execution time: " + totalFetch + "ms");
+                    // uncomment below line to run Task 5
+                    // Task5(xepEvent);
+                    // uncomment below line to run Task 6
+                    // Task6(xepEvent);
 					break;
-				case "5":
-					Console.WriteLine("How many items to generate using ADO.NET? ");				
-					String inputNum = Console.ReadLine();
-                    int numberADODOTNET;
-                    Int32.TryParse(inputNum, out numberADODOTNET);
-					if (numberADODOTNET <= 0){
-						Console.WriteLine("Number of items has to bigger than 0");
-						break;
-					}
-					//Get sample generated array to store
-					sampleArray = Trade.generateSampleData(numberADODOTNET);	
 
-					//Save generated trades using ADODOTNET
-					long totalADODOTNETStore = StoreUsingADODOTNET(xepPersister,sampleArray);
-					Console.WriteLine("Execution time: " + totalADODOTNETStore + " ms");
+                // Task 4    
+				case "5":
+                    // uncomment below line to run Task 4
+                    // Task4(sampleArray, xepPersister);
 					break;
 				case "6":
 					Console.WriteLine("Exited.");
@@ -142,6 +92,92 @@ namespace myApp
             }
         } // end main
         
+        public static void Task2CreateTrade(Trade[] sampleArray){
+            //Create trade object
+            Console.WriteLine("Stock name: ");
+            String name = Console.ReadLine();
+            
+            Console.WriteLine("Date (YYYY/MM/DD): ");
+            DateTime date;
+            if (DateTime.TryParse(Console.ReadLine(), out date)){
+                
+            }
+            else{
+                Console.WriteLine("Invalid date format!");                       
+            }  
+                                
+            Console.WriteLine("Price: ");
+            String inputPrice = Console.ReadLine();
+            double price;
+            double.TryParse(inputPrice, out price);
+            if (price <= 0){
+                Console.WriteLine("Price has to bigger than 0");
+            }
+
+            Console.WriteLine("Number of Shares: ");
+            String inputShare = Console.ReadLine();
+            int shares;
+            Int32.TryParse(inputShare, out shares);
+            if (shares <= 0){
+                Console.WriteLine("Number of Shares has to bigger than 0");
+            }
+            Console.WriteLine("Trader name: ");
+            String traderName = Console.ReadLine();
+            
+            sampleArray = CreateTrade(name, date, price, shares, traderName, sampleArray);
+        }
+        public static void Task2SaveTrade(Trade[] sampleArray, Event xepEvent){
+            //Save trades
+            Console.WriteLine("Saving trades.");
+            XEPSaveTrades(sampleArray, xepEvent);
+            sampleArray = null;
+        }
+
+        public static void Task3(Trade[] sampleArray, Event xepEvent){
+            Console.WriteLine("How many items do you want to generate? ");	
+            String inputNumber = Console.ReadLine();
+            int number;
+            Int32.TryParse(inputNumber, out number);
+            if (number <= 0){
+                Console.WriteLine("Number of items has to bigger than 0");
+            }
+            //Get sample generated array to store
+            sampleArray = Trade.generateSampleData(number);
+            
+            //Save generated trades
+            long totalStore = XEPSaveTrades(sampleArray,xepEvent);
+            Console.WriteLine("Execution time: " + totalStore + "ms");
+        }
+
+        public static void Task4(Trade[] sampleArray, EventPersister xepPersister){
+            Console.WriteLine("How many items to generate using JDBC? ");				
+            String inputNum = Console.ReadLine();
+            int numberJDBC;
+            Int32.TryParse(inputNum, out numberJDBC);
+            if (numberJDBC <= 0){
+                Console.WriteLine("Number of items has to bigger than 0");
+            }
+            //Get sample generated array to store
+            sampleArray = Trade.generateSampleData(numberJDBC);	
+
+            //Save generated trades using JDBC
+            
+            long totalJDBCStore = StoreUsingJDBC(xepPersister, sampleArray);
+            Console.WriteLine("Execution time: " + totalJDBCStore + " ms");           
+        }
+
+        public static void Task5(Event xepEvent){
+            Console.WriteLine("Fetching all. Please wait...");
+            long totalFetch = ViewAll(xepEvent);
+            Console.WriteLine("Execution time: " + totalFetch + "ms");
+        }
+
+        public static void Task6(Event xepEvent){
+            Console.WriteLine("Fetching all. Please wait...");
+            long totalFetch = ViewAllAfterUpdate(xepEvent);
+            Console.WriteLine("Execution time: " + totalFetch + "ms");
+        }
+
         public static Trade[] CreateTrade(String stockName, DateTime tDate, double price, int shares, String trader, Trade[] sampleArray)
 	    {
             Trade sampleObject = new Trade(stockName, tDate, price, shares, trader); //
@@ -174,14 +210,13 @@ namespace myApp
             return endtime - startTime;
 	    }
 
-		public static long StoreUsingADODOTNET(EventPersister persist, Trade[] sampleArray)
+		public static long StoreUsingJDBC(EventPersister persist, Trade[] sampleArray)
 		{
 			long totalTime = new long();
 			long startTime = DateTime.Now.Ticks;
 			//Loop through objects to insert
 			try {
-					
-                IRISDataAdapter da = new IRISDataAdapter();
+				IRISDataAdapter da = new IRISDataAdapter();
 				String ClassName = "Demo.Trade";
 
 				IRISADOConnection con = (IRISADOConnection) persist.GetAdoNetConnection();
@@ -224,15 +259,37 @@ namespace myApp
 	
 				
 				da.Update(ds);
-				Console.WriteLine("Inserted " + sampleArray.Length + " item(s) via ADO.NET successfully.");
+				Console.WriteLine("Inserted " + sampleArray.Length + " item(s) via JDBC successfully.");
+				
 				totalTime = DateTime.Now.Ticks - startTime;	
 			} catch (Exception e) {
-				Console.WriteLine("There was a problem storing items using ADO.NET.\n" + e);
+				Console.WriteLine("There was a problem storing items using JDBC.\n" + e);
 			}
 			return totalTime/TimeSpan.TicksPerMillisecond;
 		}
 
-		public static long ViewAll(Event xepEvent)
+
+        public static long ViewAll(Event xepEvent)
+        {
+			//Create and execute query using EventQuery
+			String sqlQuery = "SELECT * FROM MyApp.Trade WHERE purchaseprice > ? ORDER BY stockname, purchaseDate";
+			EventQuery<Trade> xepQuery = xepEvent.CreateQuery<Trade>(sqlQuery);
+			xepQuery.AddParameter(0);    // find stocks purchased > $0/share (all)
+			long startTime = DateTime.Now.Ticks;
+			xepQuery.Execute();
+
+			// Iterate through and write names of stocks using EventQueryIterator
+			Trade trade = xepQuery.GetNext();
+			while (trade != null) {
+				Console.WriteLine(trade.stockName + "\t" + trade.purchasePrice + "\t" + trade.purchaseDate);
+				trade = xepQuery.GetNext();
+			}
+			long totalTime = DateTime.Now.Ticks - startTime;
+			xepQuery.Close();
+			return totalTime/TimeSpan.TicksPerMillisecond;
+		}
+
+		public static long ViewAllAfterUpdate(Event xepEvent)
         {
 			//Create and execute query using EventQuery
 			String sqlQuery = "SELECT * FROM Demo.Trade WHERE purchaseprice > ? ORDER BY stockname, purchaseDate";
